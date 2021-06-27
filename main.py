35284ff73c83df3a29f58b6e51376fe3a9e65d41
TimPavlenko <81330963+TimPavlenko@users.noreply.github.com>
2021-06-27 22:53:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+'''
+from tim with love for paranoia and music
+
+'''
+import vk_api
+from vk_api import audio
+import requests
+import os
+
+
+REQUEST_STATUS_CODE = 200
+
+path = r"vk_music"
+
+while(True):
+    print('')
+    login = input('Enter login:')
+    password = input('Enter password:')
+    id = input('Enter your VK id:')
+    vk_session = vk_api.VkApi(login=login, password=password)
+    try:
+        vk_session.auth()
+    except vk_api.exceptions.AuthError:
+        print('')
+        print('Data is incorrect, please try again')
+        continue
+    break
+
+
+vk = vk_session.get_api()  # Теперь можно обращаться к методам API как к обычным классам
+vk_audio = audio.VkAudio(vk_session)  # Получаем доступ к audio
+
+if not os.path.exists(path):
+    os.makedirs(path)
+
+os.chdir(path)
+print('wait, connect to vk...')
+music = vk_audio.get(owner_id=id)
+music_num = len(music)
+
+score = 1
+errors = 0
+create_file = open("errors.txt", "w")
+create_file.close()
+f = open('errors.txt', 'w')
+for i in music:
+    try:
+        r = requests.get(i["url"])
+        if r.status_code == REQUEST_STATUS_CODE:
+            with open(i["artist"] + '_' + i["title"] + '.mp3', 'wb') as output_file:
+                print('[' + str(score) + '/' + str(music_num) + ']' + '  complete: ' + i["artist"] + '  |  ' + i["title"])
+                output_file.write(r.content)
+    except OSError:
+        print('[' + str(score) + '/' + str(music_num) + ']' + '  ERROR: ' + i["artist"] + '  |  ' + i["title"])
+        f.write(i["artist"] + '  |  ' + i["title"] + '\n')
+        errors += 1
+    score += 1
+print('')
+print('finish!')
+print('your music is in the vk_music folder')
+if(errors > 0):
+    print('')
+    print('all tracks that could not be downloaded are recorded in the "errors.txt" file')
